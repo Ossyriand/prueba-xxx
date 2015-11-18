a04a928c789f62d3cee141cc6c1cc4d7620817f8
Giuliano Mayer <Giuliano Mayer>
2015-11-18 11:43:38 +0100

    primera subida
    archivos adjuntos: cuenta.py y cuenta.java

---
+++ cuenta.java
@@ -0,0 +1,11 @@
+print "\nWhile controlado con Conteo"
+print "===========================\n"
+
+print "Un ejemplo es un sumador numérico hasta 10, \ncomo se muestra a continuación:\n"
+
+suma = 0
+numero = 9
+while numero >= 1:
+    suma = numero + suma
+    numero = numero - 1
+print "La suma es " + str(suma)
